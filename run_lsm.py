6b5d0588565ad79e440d657d402f663674152d9b
davispazars <davis.pazars@gmail.com>
2025-05-16 20:41:12 +0300

    Scrape last 2 days by default

--- run_lsm.py
+++ run_lsm.py
@@ -1,5 +1,6 @@
 import os
 import dotenv
+from datetime import datetime, timedelta
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
@@ -14,7 +15,11 @@ mongo_uri = os.environ.get("MONGO_URI")
 mongo_db = os.environ.get("MONGO_DB")
 mongo_collection = os.environ.get("MONGO_COLLECTION")
 
-dt_from = os.environ.get("DT_FROM", "19900101000000")
+two_days_ago = datetime.now() - timedelta(days=2)
+two_days_ago_midnight = two_days_ago.replace(hour=0, minute=0, second=0)
+dt_from = two_days_ago_midnight.strftime("%Y%m%d%H%M%S")
+if "DT_FROM" in os.environ:
+    dt_from = os.environ.get("DTFROM", "19900101000000")
 
 kwargs = {
     "mongo_uri": mongo_uri,
